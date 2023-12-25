a72140b868578f7bc1fd137b799d5510aa743e65
Maxim Alyoshin <maxim.alyosh@gmail.com>
2023-12-25 21:50:19 +0500

    feat: solve sort-by-heigth problem

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Implement class DepthCalculator with method calculateDepth
@@ -13,9 +13,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let countDepth = 1;
+    arr.forEach((item) => {
+      if (typeof item === 'object') {
+        let nextCountDepth = this.calculateDepth(item) + 1;
+        countDepth = Math.max(countDepth, nextCountDepth);
+      }
+    })
+    return countDepth;
   }
 }
 
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,10 +11,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let exceptInd = [];
+  // let arrSort = structuredClone(arr).sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] === -1) {
+      exceptInd.push(i);
+    }
+  }
+
+  if (exceptInd.length > 0) {
+    for (let i = 0; i < exceptInd.length; i += 1) {
+      arr.splice(exceptInd[i] - i, 1);
+    }
+    arr.sort((a, b) => a - b);
+    for (let i = 0; i < exceptInd.length; i += 1) {
+      arr.splice(exceptInd[i], 0, -1);
+    }
+  } else {
+    arr.sort((a, b) => a - b);
+  }
+  return arr;
 }
+
 
 module.exports = {
   sortByHeight,
